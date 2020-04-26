deb2c79edcafe00e7cc0af6873b3b77a0bb188c8
HimanshuYadav <2019kuec2009@iiitkota.ac.in>
2020-04-26 16:19:20 +0530

    Updates

---
+++ precache-manifest.68d145698a258f146eba65e6dd6f9a3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5f9d42f64fda78d7baefe11bcf50af",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "8994223be77a48071e06",
+    "url": "/Weather-App/static/css/main.e8a35da6.chunk.css"
+  },
+  {
+    "revision": "0aa290dd9cdd05387b79",
+    "url": "/Weather-App/static/js/2.40add240.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Weather-App/static/js/2.40add240.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8994223be77a48071e06",
+    "url": "/Weather-App/static/js/main.e2ecb845.chunk.js"
+  },
+  {
+    "revision": "ffe0edc8cdc3df06036d",
+    "url": "/Weather-App/static/js/runtime-main.696f3972.js"
+  },
+  {
+    "revision": "10dc1c7f103063ef5f292c1a095efaac",
+    "url": "/Weather-App/static/media/bg-image.10dc1c7f.jpg"
+  }
+]);
